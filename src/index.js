67d2bfe524c0b1f9dcfb74a80d82db66d0e3f283
briandhkim <w1057216@gmail.com>
2017-10-15 23:45:24 -0700

    initial work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// ReactDOM.render(
+//     <h1 className="container">React Prototypes!</h1>,
+//     document.getElementById('root')
+// );
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+var luckyNumber = () => Math.floor((Math.random()*1000)+1);
+var name = 'user name';
+var user ={name, luckyNumber};
+
+function greeting(user){
+	let returnDiv = (
+		<div className="container">
+			<h1> Hi, {user.name} </h1>
+			<h2 className="text-muted"> Today's number {user.luckyNumber} </h2>
+		</div>
+	);
+	return returnDiv;
+}
+
+/// const greeting = (user) =>{}
+// 	let returnDiv = (
+// 		<div className="container">
+// 			<h1> Hi, {user.name} </h1>
+// 			<h2 className="text-muted"> Today's number: {user.luckyNumber} </h2>
+// 		</div>
+// 	);
+// 	return returnDiv;
+// }
+
