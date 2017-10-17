9fde1c0ba84afd34b2ae0cfc25c0b11bc2027afd
briandhkim <w1057216@gmail.com>
2017-10-16 22:41:09 -0700

    initial edit

--- src/index.js
+++ src/index.js
@@ -13,16 +13,18 @@ ReactDOM.render(
 
 var luckyNumber = () => Math.floor((Math.random()*1000)+1);
 var name = 'user name';
-var user ={name, luckyNumber};
+// var user ={name, luckyNumber};
+var person ={
+	name : "person name",
+	luckyNumber : 0
+}
 
 function greeting(user){
-	let returnDiv = (
-		<div className="container">
-			<h1> Hi, {user.name} </h1>
-			<h2 className="text-muted"> Today's number {user.luckyNumber} </h2>
-		</div>
-	);
-	return returnDiv;
+	return <div className="container">
+				<h1> 
+					Hi, {user.name} 
+				</h1>
+			</div>
 }
 
 /// const greeting = (user) =>{}
