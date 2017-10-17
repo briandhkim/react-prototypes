a71c2404ae65bdac46bfd1c47167820db3a057a7
briandhkim <w1057216@gmail.com>
2017-10-16 22:52:28 -0700

    last edit

--- src/index.js
+++ src/index.js
@@ -1,39 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// ReactDOM.render(
-//     <h1 className="container">React Prototypes!</h1>,
-//     document.getElementById('root')
-// );
 ReactDOM.render(
     // <h1 className="container">React Prototypes!</h1>,
     greeting(user),
     document.getElementById('root')
 );
 
-var luckyNumber = () => Math.floor((Math.random()*1000)+1);
-var name = 'user name';
-// var user ={name, luckyNumber};
-var person ={
-	name : "person name",
-	luckyNumber : 0
-}
+let luckyNumber = () => Math.floor((Math.random()*1000)+1);
+let name = 'user name';
 
-function greeting(user){
-	return <div className="container">
-				<h1> 
-					Hi, {user.name} 
-				</h1>
+
+function greeting(x){
+	let returnElm = (
+			<div className="container">
+				<h1> user greeting, {x.name} </h1>
+				<h2 className="text-muted"> lucky number, {x.luckyNumber} </h2>
 			</div>
+		);
+	return returnElm;
 }
 
-/// const greeting = (user) =>{}
-// 	let returnDiv = (
-// 		<div className="container">
-// 			<h1> Hi, {user.name} </h1>
-// 			<h2 className="text-muted"> Today's number: {user.luckyNumber} </h2>
-// 		</div>
-// 	);
-// 	return returnDiv;
-// }
+var user ={
+	name : "person name",
+	luckyNumber : luckyNumber()
+}
 
